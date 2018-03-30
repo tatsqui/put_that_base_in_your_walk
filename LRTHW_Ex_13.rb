136c46fd2572f523ae6ed2d2f7535d15b995fdc1
tatsqui <34796138+tatsqui@users.noreply.github.com>
2018-03-30 11:17:32 -0400

    Create LRTHW_Ex_13.rb
    The ARGV is the "argument variable," a very standard name in programming that you will find used in many other languages. This variable holds the arguments you pass to your Ruby script when you run it. In the exercises you will get to play with this more and see what happens.
    
    Line 1 "unpacks" ARGV so that, rather than holding all the arguments, it gets assigned to three variables you can work with: first, second, and third. This may look strange, but "unpack" is probably the best word to describe what it does. It just says, "Take whatever is in ARGV, unpack it, and assign it to all of these variables on the left in order."

---
+++ LRTHW_Ex_13.rb
@@ -0,0 +1,14 @@
+#ARGV The argument variable. 
+
+first, second, third = ARGV #this line "unpacks" ARGV by assigning whatever arguments are defined to those variables. 
+
+
+puts "your first variable is #{first}."
+
+puts "your second variable is #{second}."
+
+puts "your third variable is #{third}."
+
+# This is a command line script. the arguments must be included in line when running the script from the command line. 
+# If there are three arguments in the code, then there should be three arguments when running the file, otherwise it will
+# be shown as blank.
